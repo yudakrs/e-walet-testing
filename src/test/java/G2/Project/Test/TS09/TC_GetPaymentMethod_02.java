11a2e8d08bb540abc0b715dc8689ad75b5502507
MRenaldyJ <muhammad.jovananda@dana.id>
2020-05-14 17:10:09 +0700

    added TS09

---
+++ src/test/java/G2/Project/Test/TS09/TC_GetPaymentMethod_02.java
@@ -0,0 +1,59 @@
+package G2.Project.Test.TS09;
+
+import G2.Project.Base.TestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TC_GetPaymentMethod_02 extends TestBase {
+    @BeforeClass
+    @Parameters({"invalid_token"})
+    void setUpTCGetPaymentMethod02(String token) {
+        logger.info("***** " + getClass().getName() + " *****");
+
+        RestAssured.baseURI = BaseURI;
+        httpRequest = RestAssured.given();
+
+        httpRequest.header("Authorization", "Bearer " + token);
+        response = httpRequest.request(Method.GET, "/get-payment-method");
+
+        responseBody = response.getBody().asString();
+    }
+
+    @Test
+    void checkResponseBody(){
+        checkBody();
+    }
+
+    @Test
+    void checkDataBody(){
+        checkData(true);
+    }
+
+    @Test
+    void checkStatusCode() {
+        checkStatusCode("401");
+    }
+
+    @Test
+    void checkStatusLine() {
+        checkStatusLine("HTTP/1.1 401 Unauthorized");
+    }
+
+    @Test
+    void checkResponseTime() {
+        checkResponseTime("2000");
+    }
+
+    @Test
+    void checkHeader() {
+        checkContentType();
+    }
+
+    @Test
+    void checkLength() {
+        checkContentLength("100");
+    }
+}
